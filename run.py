d1e7e794090954b3d005b15b46bfab53e5a8dcf1
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-09-28 18:20:17 +0530

    deployment changess

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from app import create_app
-
+import os
 app = create_app()
 # in create_app() or before first request
 app.jinja_env.globals.update(enumerate=enumerate)
 
 if __name__ == '__main__':
     # change host to 0.0.0.0 if you want external access
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
-    )
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
