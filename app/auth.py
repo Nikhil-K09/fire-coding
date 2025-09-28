0fa3fc862ebbd1e1b797d7ec6d9b6b8d8ff788d7
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-09-28 13:17:15 +0530

    minor changes

--- app/auth.py
+++ app/auth.py
@@ -25,21 +25,30 @@ def load_user(user_id):
     return None
 
 # Routes
+# Routes
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
 
     if request.method == 'POST':
-        username = request.form.get('username')
+        identifier = request.form.get('username')  # can be username or email
         password = request.form.get('password')
 
-        user_data = mongo.db.users.find_one({"username": username})
+        # Try to find by username first, then by email
+        user_data = mongo.db.users.find_one({
+            "$or": [
+                {"username": identifier},
+                {"email": identifier}
+            ]
+        })
+
         if user_data and check_password_hash(user_data['password'], password):
             user = User(user_data)
             login_user(user)
             return redirect(url_for('main.index'))
-        flash("Invalid username or password", "danger")
+
+        flash("Invalid username/email or password", "danger")
 
     return render_template('login.html')
 
@@ -51,15 +60,26 @@ def register():
 
     if request.method == 'POST':
         username = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
 
+        # Ensure unique username and email
         if mongo.db.users.find_one({"username": username}):
             flash("Username already exists", "danger")
             return redirect(url_for('auth.register'))
 
+        if mongo.db.users.find_one({"email": email}):
+            flash("Email already registered", "danger")
+            return redirect(url_for('auth.register'))
+
         hashed_pw = generate_password_hash(password)
-        user_id = mongo.db.users.insert_one({"username": username, "password": hashed_pw}).inserted_id
-        user = User({"_id": user_id, "username": username})
+        user_id = mongo.db.users.insert_one({
+            "username": username,
+            "email": email,
+            "password": hashed_pw
+        }).inserted_id
+
+        user = User({"_id": user_id, "username": username, "email": email})
         login_user(user)
         return redirect(url_for('main.index'))
 
